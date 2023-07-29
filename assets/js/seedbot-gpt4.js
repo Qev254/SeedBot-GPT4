c6914165b32157b54c1a014bc6dc459a972fadb8
Qev254 <kelvinkkaranja@gmail.com>
2023-07-29 03:20:57 +0300

    From LOCAL-Update seedbot-gpt4.js

--- assets/js/seedbot-gpt4.js
+++ assets/js/seedbot-gpt4.js
@@ -1,51 +1,52 @@
 jQuery(document).ready(function ($) {
 
-    // We log for Diagnostics - Ver 1.4.2
+    // Logging for Diagnostics - Ver 1.4.2
     var seedbot_gpt4_diagnostics = localStorage.getItem('seedbot_gpt4_diagnostics') || 'Off';
     localStorage.setItem('seedbot_gpt4_diagnostics', seedbot_gpt4_diagnostics); // Set if not set
 
-    var seedbot_gpt4_messageInput = $('#seedbot-gpt4-message');
+    var seedbot_gpt4_messageInput = $('#seedbot_gpt4_message');
     var seedbot_gpt4_conversation = $('#seedbot-gpt4-conversation');
     var seedbot_gpt4_submitButton = $('#seedbot-gpt4-submit');
 
     // Set bot width with the default Narrow or from setting Wide - Ver 1.4.2
-    var seedbot_gpt4_chatgpt_width_setting = localStorage.getItem('seedbot_gpt4_chatgpt_width_setting') || 'Narrow';
+    var seedbot_gpt4_width_setting = localStorage.getItem('seedbot_gpt4_width_setting') || 'Narrow';
 
-    var seedbot_gpt4_chatGptChatBot = $('#seedbot-gpt4-chatgpt');
-    if (seedbot_gpt4_chatgpt_width_setting === 'Wide') {
-        seedbot_gpt4_chatGptChatBot.addClass('wide');
+    var seedbot_gpt4_ChatBot = $('#seedbot-gpt4-chatbot');
+    if (seedbot_gpt4_width_setting === 'Wide') {
+        seedbot_gpt4_ChatBot.addClass('wide');
     } else {
-        seedbot_gpt4_chatGptChatBot.removeClass('wide');
+        seedbot_gpt4_ChatBot.removeClass('wide');
     }
 
     // Diagnostics = Ver 1.4.2
-    if (seedbot_gpt4_chatgpt_diagnostics === 'On') {
-        console.log(seedbot_gpt4_messageInput);
-        console.log(seedbot_gpt4_conversation);
-        console.log(seedbot_gpt4_submitButton);
-        console.log(seedbot_gpt4_chatGptChatBot);
-        console.log('seedbot_gpt4_chatgpt_width_setting: ' + seedbot_gpt4_chatgpt_width_setting);
+    if (seedbotSettings.seedbot_gpt4_diagnostics === 'On') {
+        console.log('FUNCTION: seedbot-gpt4.js');
+        console.log('seedbot_gpt4_messageInput: ' + seedbot_gpt4_messageInput);
+        console.log('seedbot_gpt4_conversation' + seedbot_gpt4_conversation);
+        console.log('seedbot_gpt4_submitButton: ' + seedbot_gpt4_submitButton);
+        console.log('seedbot_gpt4_ChatBot: ' + seedbot_gpt4_ChatBot);
+        console.log('seedbot_gpt4_width_setting: ' + seedbot_gpt4_width_setting);
     }
 
-    var seedbot_gpt4_chatGptOpenButton = $('#seedbot-gpt4-open-btn');
+    var seedbot_gpt4_OpenButton = $('#seedbot-gpt4-open-btn');
     // Use 'open' for an open chatbot or 'closed' for a closed chatbot - Ver 1.1.0
-    var seedbot_gpt4_chatgpt_start_status = 'closed';
+    var seedbot_gpt4_start_status = 'closed';
     
     // Initially hide the chatbot - Ver 1.1.0
-    seedbot_gpt4_chatGptChatBot.hide();
-    seedbot_gpt4_chatGptOpenButton.show();
+    seedbot_gpt4_ChatBot.hide();
+    seedbot_gpt4_OpenButton.show();
 
     var seedbot_gpt4_chatbotContainer = $('<div></div>').addClass('chatbot-container');
     var seedbot_gpt4_chatbotCollapseBtn = $('<button></button>').addClass('chatbot-collapse-btn').addClass('dashicons dashicons-format-chat'); // Add a collapse button
     var seedbot_gpt4_chatbotCollapsed = $('<div></div>').addClass('chatbot-collapsed'); // Add a collapsed chatbot icon dashicons-format-chat f125
 
     // Support variable greetings based on setting - Ver 1.1.0
-    var seedbot_gpt4_initialGreeting = localStorage.getItem('seedbot_gpt4_chatgpt_initial_greeting') || 'Hello! How can I help you today?';
-    localStorage.setItem('seedbot_gpt4_chatgpt_initial_greeting', seedbot_gpt4_initialGreeting);
-    var seedbot_gpt4_subsequentGreeting = localStorage.getItem('seedbot_gpt4_chatgpt_subsequent_greeting') || 'Hello again! How can I help you?';
-    localStorage.setItem('seedbot_gpt4_chatgpt_subsequent_greeting', seedbot_gpt4_subsequentGreeting);
+    var seedbot_gpt4_initialGreeting = localStorage.getItem('seedbot_gpt4_initial_greeting') || 'Hello! How can I help you today?';
+    localStorage.setItem('seedbot_gpt4_initial_greeting', seedbot_gpt4_initialGreeting);
+    var seedbot_gpt4_subsequentGreeting = localStorage.getItem('seedbot_gpt4_subsequent_greeting') || 'Hello again! How can I help you?';
+    localStorage.setItem('seedbot_gpt4_subsequent_greeting', seedbot_gpt4_subsequentGreeting);
     // Handle disclaimer - Ver 1.4.1
-    var seedbot_gpt4_chatgpt_disclaimer_setting = localStorage.getItem('seedbot_gpt4_chatgpt_disclaimer_setting') || 'Yes';
+    var seedbot_gpt4_disclaimer_setting = localStorage.getItem('seedbot_gpt4_disclaimer_setting') || 'Yes';
 
     // Append the collapse button and collapsed chatbot icon to the chatbot container
     seedbot_gpt4_chatbotContainer.append(seedbot_gpt4_chatbotCollapseBtn);
@@ -55,17 +56,17 @@ jQuery(document).ready(function ($) {
     seedbot_gpt4_conversation.append(seedbot_gpt4_chatbotContainer);
 
     function seedbot_gpt4_initializeChatbot() {
-        var seedbot_gpt4_chatgpt_diagnostics = localStorage.getItem('seedbot_gpt4_chatgpt_diagnostics') || 'Off';
-        var seedbot_gpt4_isFirstTime = !localStorage.getItem('seedbot_gpt4_chatgptChatbotOpened');
+        var seedbot_gpt4_diagnostics = localStorage.getItem('seedbot_gpt4_diagnostics') || 'Off';
+        var seedbot_gpt4_isFirstTime = !localStorage.getItem('seedbot_gpt4_ChatBotOpened');
         var seedbot_gpt4_initialGreeting;
         // Remove any legacy conversations that might be store in local storage for increased privacy - Ver 1.4.2
-        localStorage.removeItem('seedbot_gpt4_chatgpt_conversation');
+        localStorage.removeItem('seedbot_gpt4_conversation');
   
         if (seedbot_gpt4_isFirstTime) {
-            seedbot_gpt4_initialGreeting = localStorage.getItem('seedbot_gpt4_chatgpt_initial_greeting') || 'Hello! How can I help you today?';
+            seedbot_gpt4_initialGreeting = localStorage.getItem('seedbot_gpt4_initial_greeting') || 'Hello! How can I help you today?';
 
             // Logging for Diagnostics - Ver 1.4.2
-            if (seedbot_gpt4_chatgpt_diagnostics === 'On') {
+            if (seedbot_gpt4_diagnostics === 'On') {
                 console.log("initialGreeting" . seedbot_gpt4_initialGreeting);
             }
 
@@ -75,16 +76,16 @@ jQuery(document).ready(function ($) {
             }
 
             seedbot_gpt4_appendMessage(seedbot_gpt4_initialGreeting, 'bot', 'initial-greeting');
-            localStorage.setItem('seedbot_gpt4_chatgptChatbotOpened', 'true');
+            localStorage.setItem('seedbot_gpt4_ChatBotOpened', 'true');
             // Save the conversation after the initial greeting is appended - Ver 1.2.0
-            sessionStorage.setItem('seedbot_gpt4_chatgpt_conversation', seedbot_gpt4_conversation.html());
+            sessionStorage.setItem('seedbot_gpt4_conversation', seedbot_gpt4_conversation.html());
 
         } else {
             
-            seedbot_gpt4_initialGreeting = localStorage.getItem('seedbot_gpt4_chatgpt_subsequent_greeting') || 'Hello again! How can I help you?';
+            seedbot_gpt4_initialGreeting = localStorage.getItem('seedbot_gpt4_subsequent_greeting') || 'Hello again! How can I help you?';
 
             // Logging for Diagnostics - Ver 1.4.2
-            if (seedbot_gpt4_chatgpt_diagnostics === 'On') {
+            if (seedbot_gpt4_diagnostics === 'On') {
                 console.log("initialGreeting" . seedbot_gpt4_initialGreeting);
             }
 
@@ -94,14 +95,14 @@ jQuery(document).ready(function ($) {
             }
             
             seedbot_gpt4_appendMessage(seedbot_gpt4_initialGreeting, 'bot', 'initial-greeting');
-            localStorage.setItem('seedbot_gpt4_chatgptChatbotOpened', 'true');
+            localStorage.setItem('seedbot_gpt4_ChatBotOpened', 'true');
         }
     }
 
 
     // Add chatbot header, body, and other elements - Ver 1.1.0
     var seedbot_gpt4_chatbotHeader = $('<div></div>').addClass('chatbot-header');
-    seedbot_gpt4_chatGptChatBot.append(seedbot_gpt4_chatbotHeader);
+    seedbot_gpt4_ChatBot.append(seedbot_gpt4_chatbotHeader);
 
     // Fix for Ver 1.2.0
     seedbot_gpt4_chatbotHeader.append(seedbot_gpt4_chatbotCollapseBtn);
@@ -110,7 +111,7 @@ jQuery(document).ready(function ($) {
     // Attach the click event listeners for the collapse button and collapsed chatbot icon
     seedbot_gpt4_chatbotCollapseBtn.on('click', seedbot_gpt4_toggleChatbot);
     seedbot_gpt4_chatbotCollapsed.on('click', seedbot_gpt4_toggleChatbot);
-    seedbot_gpt4_chatGptOpenButton.on('click', seedbot_gpt4_toggleChatbot);
+    seedbot_gpt4_OpenButton.on('click', seedbot_gpt4_toggleChatbot);
 
     function seedbot_gpt4_appendMessage(message, sender, cssClass) {
         var messageElement = $('<div></div>').addClass('chat-message');
@@ -146,7 +147,7 @@ jQuery(document).ready(function ($) {
         seedbot_gpt4_conversation[0].scrollTop = seedbot_gpt4_conversation[0].scrollHeight;
 
         // Save the conversation locally between bot sessions - Ver 1.2.0
-        sessionStorage.setItem('seedbot_gpt4_chatgpt_conversation', seedbot_gpt4_conversation.html());
+        sessionStorage.setItem('seedbot_gpt4_conversation', seedbot_gpt4_conversation.html());
 
     }
 
@@ -167,7 +168,7 @@ jQuery(document).ready(function ($) {
 
     seedbot_gpt4_submitButton.on('click', function () {
         var message = seedbot_gpt4_messageInput.val().trim();
-        var seedbot_gpt4_chatgpt_disclaimer_setting = localStorage.getItem('seedbot_gpt4_chatgpt_disclaimer_setting') || 'Yes';
+        var seedbot_gpt4_disclaimer_setting = localStorage.getItem('seedbot_gpt4_disclaimer_setting') || 'Yes';
 
         if (!message) {
             return;
@@ -177,10 +178,10 @@ jQuery(document).ready(function ($) {
         seedbot_gpt4_appendMessage(message, 'user');
 
         $.ajax({
-            url: seedbot_gpt4_chatgpt_params.ajax_url,
+            url: seedbot_gpt4_params.ajax_url,
             method: 'POST',
             data: {
-                action: 'seedbot_gpt4_chatgpt_send_message',
+                action: 'seedbot_gpt4_send_message',
                 message: message,
             },
             beforeSend: function () {
@@ -194,9 +195,9 @@ jQuery(document).ready(function ($) {
                     const prefix_a = "As an AI language model, ";
                     const prefix_b = "I am an AI language model and ";
 
-                    if (botResponse.startsWith(prefix_a) && seedbot_gpt4_chatgpt_disclaimer_setting === 'No') {
+                    if (botResponse.startsWith(prefix_a) && seedbot_gpt4_disclaimer_setting === 'No') {
                         botResponse = botResponse.slice(prefix_a.length);
-                    } else if (botResponse.startsWith(prefix_b) && seedbot_gpt4_chatgpt_disclaimer_setting === 'No') {
+                    } else if (botResponse.startsWith(prefix_b) && seedbot_gpt4_disclaimer_setting === 'No') {
                         botResponse = botResponse.slice(prefix_b.length);
                     }
                                     
@@ -225,17 +226,17 @@ jQuery(document).ready(function ($) {
 
     // Add the seedbot_gpt4_toggleChatbot() function - Ver 1.1.0
     function seedbot_gpt4_toggleChatbot() {
-        if (seedbot_gpt4_chatGptChatBot.is(':visible')) {
-            seedbot_gpt4_chatGptChatBot.hide();
-            seedbot_gpt4_chatGptOpenButton.show();
-            localStorage.setItem('seedbot_gpt4_chatGPTChatBotStatus', 'closed');
+        if (seedbot_gpt4_ChatBot.is(':visible')) {
+            seedbot_gpt4_ChatBot.hide();
+            seedbot_gpt4_OpenButton.show();
+            localStorage.setItem('seedbot_gpt4_ChatBotStatus', 'closed');
             // Clear the conversation when the chatbot is closed - Ver 1.2.0
             // Keep the conversation when the chatbot is closed - Ver 1.2.4
-            // sessionStorage.removeItem('seedbot_gpt4_chatgpt_conversation');
+            // sessionStorage.removeItem('seedbot_gpt4_conversation');
         } else {
-            seedbot_gpt4_chatGptChatBot.show();
-            seedbot_gpt4_chatGptOpenButton.hide();
-            localStorage.setItem('seedbot_gpt4_chatGPTChatBotStatus', 'open');
+            seedbot_gpt4_ChatBot.show();
+            seedbot_gpt4_OpenButton.hide();
+            localStorage.setItem('seedbot_gpt4_ChatBotStatus', 'open');
             seedbot_gpt4_loadConversation();
             seedbot_gpt4_scrollToBottom();
         }
@@ -243,30 +244,30 @@ jQuery(document).ready(function ($) {
 
     // Add this function to maintain the chatbot status across page refreshes and sessions - Ver 1.1.0 and updated for Ver 1.4.1
     function seedbot_gpt4_loadChatbotStatus() {
-        const seedbot_gpt4_chatGPTChatBotStatus = localStorage.getItem('seedbot_gpt4_chatGPTChatBotStatus');
-        // const seedbot_gpt4_chatGPTChatBotStatus = localStorage.getItem('seedbot_gpt4_chatgpt_start_status');
+        const seedbot_gpt4_ChatBotStatus = localStorage.getItem('seedbot_gpt4_ChatBotStatus');
+        // const seedbot_gpt4_ChatBotStatus = localStorage.getItem('seedbot_gpt4_start_status');
         
-        // If the chatbot status is not set in local storage, use seedbot_gpt4_chatgpt_start_status
-        if (seedbot_gpt4_chatGPTChatBotStatus === null) {
-            if (seedbot_gpt4_chatgpt_start_status === 'closed') {
-                seedbot_gpt4_chatGptChatBot.hide();
-                seedbot_gpt4_chatGptOpenButton.show();
+        // If the chatbot status is not set in local storage, use seedbot_gpt4_start_status
+        if (seedbot_gpt4_ChatBotStatus === null) {
+            if (seedbot_gpt4_start_status === 'closed') {
+                seedbot_gpt4_ChatBot.hide();
+                seedbot_gpt4_OpenButton.show();
             } else {
-                seedbot_gpt4_chatGptChatBot.show();
-                seedbot_gpt4_chatGptOpenButton.hide();
+                seedbot_gpt4_ChatBot.show();
+                seedbot_gpt4_OpenButton.hide();
                 // Load the conversation when the chatbot is shown on page load
                 seedbot_gpt4_loadConversation();
                 seedbot_gpt4_scrollToBottom();
             }
-        } else if (seedbot_gpt4_chatGPTChatBotStatus === 'closed') {
-            if (seedbot_gpt4_chatGptChatBot.is(':visible')) {
-                seedbot_gpt4_chatGptChatBot.hide();
-                seedbot_gpt4_chatGptOpenButton.show();
+        } else if (seedbot_gpt4_ChatBotStatus === 'closed') {
+            if (seedbot_gpt4_ChatBot.is(':visible')) {
+                seedbot_gpt4_ChatBot.hide();
+                seedbot_gpt4_OpenButton.show();
             }
-        } else if (seedbot_gpt4_chatGPTChatBotStatus === 'open') {
-            if (seedbot_gpt4_chatGptChatBot.is(':hidden')) {
-                seedbot_gpt4_chatGptChatBot.show();
-                seedbot_gpt4_chatGptOpenButton.hide();
+        } else if (seedbot_gpt4_ChatBotStatus === 'open') {
+            if (seedbot_gpt4_ChatBot.is(':hidden')) {
+                seedbot_gpt4_ChatBot.show();
+                seedbot_gpt4_OpenButton.hide();
                 seedbot_gpt4_loadConversation();
                 seedbot_gpt4_scrollToBottom();
             }
@@ -277,7 +278,7 @@ jQuery(document).ready(function ($) {
     function seedbot_gpt4_scrollToBottom() {
         setTimeout(() => {
             // Logging for Diagnostics - Ver 1.4.2
-            if (seedbot_gpt4_chatgpt_diagnostics === 'On') {
+            if (seedbot_gpt4_diagnostics === 'On') {
                 console.log("Scrolling to bottom");
                 console.log("Scroll height: " + seedbot_gpt4_conversation[0].scrollHeight);
             }
@@ -287,7 +288,7 @@ jQuery(document).ready(function ($) {
    
     // Load conversation from local storage if available - Ver 1.2.0
     function seedbot_gpt4_loadConversation() {
-        var seedbot_gpt4_storedConversation = sessionStorage.getItem('seedbot_gpt4_chatgpt_conversation');
+        var seedbot_gpt4_storedConversation = sessionStorage.getItem('seedbot_gpt4_conversation');
         if (seedbot_gpt4_storedConversation) {
             seedbot_gpt4_conversation.append(seedbot_gpt4_storedConversation);
             // Use setTimeout to ensure seedbot_gpt4_scrollToBottom is called after the conversation is rendered
