735470592fa174f50ea9e088f5d92096fe00bf69
Qev254 <kelvinkkaranja@gmail.com>
2023-08-04 01:29:40 +0300

    reset

--- seedbot-gpt4.php
+++ seedbot-gpt4.php
@@ -2,8 +2,8 @@
 /*
  * Plugin Name: SeedBot GPT4
  * Plugin URI:  https://github.com/qev254/seedbot-gpt4
- * Description: A simple plugin to add a SeedBot GPT4 chatbot to your WordPress Website.
- * Version:     1.0.0
+ * Description: A smple plugin to add a SeedBot GPT4 chatbot to your WordPress Website.
+ * Version:     1.0
  * Author:      joxdigital.com
  * Author URI:  https://www.joxdigital.com
  * License:     GPLv2 or later
@@ -39,14 +39,13 @@ update_option('seedbot_diagnostics', 'Off');
 // Enqueue plugin scripts and styles
 function seedbot_gpt4_enqueue_scripts()
 {
-    // Ensure the Dashicons font is properly enqueued - Ver 1.1.0
+    // Ensure the Dashicons font is properly enqueued - Ver 1.0
     wp_enqueue_style('dashicons');
     wp_enqueue_style('seedbot-gpt4-css', plugins_url('assets/css/seedbot-gpt4.css', __FILE__));
-    wp_enqueue_script('seedbot-gpt4-js', plugins_url('assets/js/seedbot-gpt4.js', __FILE__), array('jquery'), '1.0', true);
+    wp_enqueue_script('seedbot-gpt4-js', plugins_url('assets/js/seedbot-gpt4.js', __FILE__));
 
-    // Ver 1.4.1
-    // Enqueue the seedbot-gpt4-local.js file
-    wp_enqueue_script('seedbot-gpt4-local', plugins_url('assets/js/seedbot-gpt4-local.js', __FILE__), array('jquery'), '1.0', true);
+    // Enqueue the seedbot-gpt4-local.js file as seedbot-gpt4-local
+    wp_enqueue_script('seedbot-gpt4-local', plugins_url('assets/js/seedbot-gpt4-local.js', __FILE__), array('jquery'));
     $seedbot_settings = array(
         'seedbot_bot_name' => esc_attr(get_option('seedbot_bot_name')),
         'seedbot_initial_greeting' => esc_attr(get_option('seedbot_initial_greeting')),
@@ -63,19 +62,41 @@ function seedbot_gpt4_enqueue_scripts()
         'api_key' => esc_attr(get_option('seedbot_api_key')),
     ));
     echo "<script>console.log('{$seedbot_settings[0]}' );</script>";
-    
-    // echo "<script>console.log('tesooot' );</script>";
+        echo "<script>console.log('here' );</script>";
+
+    echo "<script>console.log('tesooot');</script>";
+    echo "<script>console.log('tesooooot');</script>";
+
 }
 add_action('wp_enqueue_scripts', 'seedbot_gpt4_enqueue_scripts');
+seedebug_to_console($seedbot_settings);
+
+$testingdeb = 'seedbot_bot_name';
+seedebug_to_console($testingdeb);
+
+
+//Logging in Dev
+function seedebug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+$debug_data = "TESTING DEBUGGER";
+seedebug_to_console($debug_data);
 
 // Handle Ajax requests
 function seedbot_gpt4_send_message()
 {
     // Get the save API key
     $api_key = esc_attr(get_option('seedbot_api_key'));
+
     // Get the saved model from the settings or default to gpt-3.5-turbo
     $model = esc_attr(get_option('seedbot_model_choice', 'gpt-3.5-turbo'));
-    // Max tokens - Ver 1.4.2
+    seedebug_to_console($model);
+
+    // Max tokens - 
     $max_tokens = esc_attr(get_option('seedbot_max_tokens_setting', 150));
     // Send only clean text via the API
     $message = sanitize_text_field($_POST['message']);
